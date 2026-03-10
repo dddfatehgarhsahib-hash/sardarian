0f8b3412ba828009fa18fd4e34f5286eeac0c529
dddfatehgarhsahib-hash <dddfatehgarhsahib@gmail.com>
2026-03-10 15:13:46 +0530

    Add Home component for event registration page

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+export default function Home(){
+
+return(
+
+<div style={{textAlign:"center",padding:"40px"}}>
+
+<h1>ਪੰਜਵਾਂ ਦਸਤਾਰ ਤੇ ਦੁਮਾਲਾ ਮੁਕਾਬਲਾ</h1>
+
+<h3>ਸਰਦਾਰੀਆਂ ਯੂਥ ਕਲੱਬ ਪਟਿਆਲਾ</h3>
+
+<p>
+
+18 ਅਪ੍ਰੈਲ 2026<br/>
+
+ਗੁਰਦੁਆਰਾ ਸ਼੍ਰੀ ਦੂਖਨਿਵਾਰਨ ਸਾਹਿਬ ਪਟਿਆਲਾ
+
+</p>
+
+<b>ਸਿਰਫ ਪਟਿਆਲਾ ਜਿਲ੍ਹੇ ਦੇ ਬੱਚਿਆਂ ਲਈ</b>
+
+<br/><br/>
+
+<a href="/register">
+
+<button style={{
+padding:"15px",
+fontSize:"18px",
+background:"darkred",
+color:"white",
+borderRadius:"8px"
+}}>
+
+Register Now
+
+</button>
+
+</a>
+
+</div>
+
+)
+
+}
