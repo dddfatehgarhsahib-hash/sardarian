4ee026c498285a95e2ac45efe2a2ffeecdadf8ea
dddfatehgarhsahib-hash <dddfatehgarhsahib@gmail.com>
2026-03-10 15:18:23 +0530

    Create Admin component for managing registrations

---
+++ pages/admin.js
@@ -0,0 +1,39 @@
+export default function Admin(){
+
+return(
+
+<div style={{padding:"40px"}}>
+
+<h1>Admin Panel</h1>
+
+<p>All registrations will appear here.</p>
+
+<table border="1" cellPadding="10">
+
+<tr>
+
+<th>Token</th>
+<th>Name</th>
+<th>Father</th>
+<th>Phone</th>
+<th>Category</th>
+
+</tr>
+
+<tr>
+
+<td>DKP-001</td>
+<td>Example</td>
+<td>Example</td>
+<td>9876543210</td>
+<td>7-15</td>
+
+</tr>
+
+</table>
+
+</div>
+
+)
+
+}
